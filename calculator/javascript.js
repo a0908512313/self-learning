3138ad23e9ed32e3c49ecbd51d688a782a6c9d08
a0908512313 <a0908512313@gmail.com>
2025-01-14 12:53:05 +0800

    calculator temp

---
+++ calculator/javascript.js
@@ -0,0 +1,23 @@
+const main = document.getElementById("main");
+const history = document.getElementById("history");
+const buttons = document.querySelectorAll("button");
+
+buttons.forEach((button) => {
+	button.addEventListener("click", () => {
+		const value = button.innerText;
+		if (value != "=") {
+			history.innerText += value;
+		}
+		if (!isNaN(value)) {
+			if (main.innerText == "0") {
+				history.innerText = value;
+				main.innerText = value;
+			} else {
+				main.innerText += value;
+			}
+		} else if (value == "CE") {
+			main.innerText = "0";
+			history.innerText = "";
+		}
+	});
+});
